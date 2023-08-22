352fdcc6a35b013f8ad525ea176e0caa2303528a
created.arkjuniork <created.arkjuniork@gmail.com>
2023-08-22 14:11:38 +0800

    feat: update packages & config

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,16 +1,17 @@
 package cmd
 
-import "flag"
+import (
+	"flag"
+	"github.com/ArkjuniorK/gofiber-boilerplate/internal/app"
+)
 
 // Flag set to use by the app
 var (
 	fs flag.FlagSet
 
-	port = fs.Int("port", 8080, "Port to use by the app")
+	port = fs.String("port", "8080", "Port to use by the app")
 	host = fs.String("host", "localhost", "Host to use by the app")
 )
 
 // This function use to initialize, configure and run the entire app.
-func init() {
-
-}
+func init() { app.New(*host, *port) }
