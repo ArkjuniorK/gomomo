47f4f1b2773eb1926f042d4e05438c683111d993
created.arkjuniork <created.arkjuniork@gmail.com>
2023-11-23 00:55:30 +0800

    feat: add ReplaceAttr function to slog

--- internal/core/logger.go
+++ internal/core/logger.go
@@ -3,6 +3,7 @@ package core
 import (
 	"log/slog"
 	"os"
+	"path/filepath"
 )
 
 // Logger act application logging using slog, standard Go library for
@@ -13,7 +14,7 @@ type Logger struct {
 }
 
 func NewLogger() *Logger {
-	opts := &slog.HandlerOptions{AddSource: true}
+	opts := &slog.HandlerOptions{AddSource: true, ReplaceAttr: replaceAttr}
 	hdlr := slog.NewTextHandler(os.Stdout, opts)
 	core := slog.New(hdlr)
 
@@ -22,4 +23,24 @@ func NewLogger() *Logger {
 
 func (l *Logger) GetCore() *slog.Logger {
 	return l.core
+}
+
+func replaceAttr(groups []string, a slog.Attr) slog.Attr {
+	// Replace full path of source file to workdir path
+	if a.Key == slog.SourceKey {
+		source := a.Value.Any().(*slog.Source)
+
+		wd, err := os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+
+		file, err := filepath.Rel(wd, source.File)
+		if err != nil {
+			panic(err)
+		}
+
+		source.File = file
+	}
+	return a
 }
