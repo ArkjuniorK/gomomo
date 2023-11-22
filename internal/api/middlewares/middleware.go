79c9b5c832a27d4078d3a8bf47cb1c56ba2be6ed
created.arkjuniork <created.arkjuniork@gmail.com>
2023-11-23 00:56:32 +0800

    feat: add ResponseDispatcher middleware responsible to send response to client

--- internal/api/middlewares/middleware.go
+++ internal/api/middlewares/middleware.go
@@ -1,4 +1,25 @@
 package middlewares
 
-// Define global middleware here, for module specific middleware
-// its more convenient to declared at other file contain module name
+import (
+	"github.com/ArkjuniorK/gofiber-boilerplate/internal/api/schema"
+	"github.com/gofiber/fiber/v2"
+)
+
+// Define global middleware here, for package's specific middleware
+// its more convenient to declared at other file with the same name
+
+func ResponseDispatcher() fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		if err := ctx.Next(); err != nil {
+			return err
+		}
+
+		data := ctx.UserContext().Value("data")
+
+		rs := new(schema.Response)
+		rs.Msg = "success"
+		rs.Data = data
+
+		return ctx.JSON(rs)
+	}
+}
